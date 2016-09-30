06ab9e92714405e8f63c60cad3fbb5cc31a0b8e2
ijkoh1 <ijkoh1@student.monash.edu>
2016-09-30 12:42:25 +0800

    init

---
+++ app/src/main/java/com/example/ivan/myapplication/LittleProlog.java
@@ -0,0 +1,14 @@
+package com.example.ivan.myapplication;
+
+/**
+ * Created by Ivan on 9/24/2016.
+ */
+public class LittleProlog {
+    public static void main(String[] args){
+
+    }
+
+    public void startApplication(){
+        
+    }
+}
