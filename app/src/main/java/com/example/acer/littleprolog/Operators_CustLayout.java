e8848113ee6732124ebdd8e120dcd9f0fbb51200
Chuah Seong Han <seonghan94@gmail.com>
2016-10-03 14:53:44 +0800

    Added GUI Functions (Part1)

---
+++ app/src/main/java/com/example/acer/littleprolog/Operators_CustLayout.java
@@ -0,0 +1,69 @@
+package com.example.acer.littleprolog;
+
+/**
+ * Created by Seong Han on 10/2/2016.
+ */
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.ivan.myapplication.MainActivity;
+
+public class Operators_CustLayout extends RelativeLayout {
+
+    private EditText edit_box1,edit_box2;
+    private TextView more_eq_sign,less_eq_sign,more_than_sign,less_than_sign,
+            eq,add,minus,mult,divide;
+
+    public Operators_CustLayout(Context context){
+        super(context);
+        init();
+    }
+
+    public Operators_CustLayout(Context context, AttributeSet attrs){
+        super(context,attrs);
+        init();
+    }
+
+    public Operators_CustLayout(Context context, AttributeSet attrs, int defStyle){
+        super(context,attrs,defStyle);
+        init();
+    }
+
+    private void init() {
+
+        inflate(getContext(),R.layout.more_equal,this);
+        inflate(getContext(),R.layout.less_equal,this);
+        inflate(getContext(),R.layout.less_than,this);
+        inflate(getContext(),R.layout.more_than,this);
+        inflate(getContext(),R.layout.equal,this);
+        inflate(getContext(),R.layout.add,this);
+        inflate(getContext(),R.layout.minus,this);
+        inflate(getContext(),R.layout.multiply,this);
+        inflate(getContext(),R.layout.divide,this);
+
+        setBackgroundColor(getResources().getColor(R.color.operatorsColor));
+
+        int padding_5dp = (int)getResources().getDimension(R.dimen.operators_padding);
+        setPadding(padding_5dp,padding_5dp,padding_5dp,padding_5dp);
+
+        this.edit_box1 = (EditText)findViewById(R.id.empty1);
+        this.edit_box2 = (EditText)findViewById(R.id.empty2);
+        this.more_eq_sign = (TextView)findViewById(R.id.more_eq);
+        this.less_eq_sign = (TextView)findViewById(R.id.less_eq);
+        this.more_than_sign = (TextView)findViewById(R.id.more_sign);
+        this.less_than_sign = (TextView)findViewById(R.id.less_sign);
+        this.more_eq_sign = (TextView)findViewById(R.id.more_eq);
+        this.eq = (TextView)findViewById(R.id.eq_sign);
+        this.add = (TextView)findViewById(R.id.add_sign);
+        this.minus = (TextView)findViewById(R.id.minus_sign);
+        this.mult = (TextView)findViewById(R.id.mult_sign);
+        this.divide = (TextView)findViewById(R.id.div_sign);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/acer/littleprolog/PopUp_Menu.java
@@ -0,0 +1,81 @@
+package com.example.acer.littleprolog;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Process;
+import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.view.MenuItem;
+
+/**
+ * Created by Seong Han on 10/2/2016.
+ */
+
+public class PopUp_Menu extends LinearLayout {
+
+    /*@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.delete_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(DeleteButton.this, view);
+                popupMenu.setOnMenuItemClickListener(DeleteButton.this);
+                popupMenu.inflate(R.menu.delete_popup_menu);
+                popupMenu.show();
+            }
+        });
+    }*/
+
+    private Button deleteButton, openButton, toolsButton;
+
+    public PopUp_Menu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        this.deleteButton = (Button) findViewById(R.id.clear_button);
+        this.openButton = (Button) findViewById(R.id.open_button);
+        this.toolsButton = (Button) findViewById(R.id.tools_button);
+    }
+}
+
+   /* public boolean onMenuItemClick(MenuItem item) {
+
+        final ScrollView editorBox = (ScrollView) findViewById(R.id.editor1);
+        final ScrollView consoleBox = (ScrollView) findViewById(R.id.console);
+
+        switch (item.getItemId()) {
+            case R.id.deleteEditor:
+                if((editorBox).getChildCount() > 0)
+                    (editorBox).removeAllViews();
+                return true;
+
+            case R.id.deleteConsole:
+                if((consoleBox).getChildCount() > 0)
+                    (consoleBox).removeAllViews();
+                return true;
+
+            case R.id.deleteAll:
+
+                if((editorBox).getChildCount() > 0)
+                    (editorBox).removeAllViews();
+
+                if((consoleBox).getChildCount() > 0)
+                    (consoleBox).removeAllViews();
+
+                return true;
+        }
+        return true;
+    }
+}*/
